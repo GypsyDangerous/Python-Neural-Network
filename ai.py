e0782f9f0de6453d4f07b2ca6cf9990f7204b364
David Snyder <45735850+GypsyDangerous@users.noreply.github.com>
2019-03-14 21:19:29 -0400

    Update ai.py

--- ai.py
+++ ai.py
@@ -1,10 +1,3 @@
-import numpy as np
-import matplotlib.pyplot as plt
-from perceptron import *
-
-def truncate(x):
-	return int(x*100)/100
-
 def percent(x, total):
 	return truncate((x/total)*100)
 
@@ -13,29 +6,34 @@ dataset = [[1,0,1],
 			[1,1,0],
 			[0,0,0]]
 
-brain = perceptron(2, 2, 100, 1)
-epochs = 20000
+brain = perceptron(2, 1, 50, 1, 20000)
+epochs = brain.getEpochs()
 
 brain.setLearningRate(.1)
+datasize = len(dataset)
 
 errors = []
 eps = []
 
 # training loop
 for i in range(epochs):
-	index = np.random.randint(len(dataset))
+	index = np.random.randint(datasize)
 	data = dataset[index]
 	info = [data[0], data[1]]
 	goal = [data[2]]
 	brain.train(info, goal)
 	errors.append(brain.mse(info, goal))
-	eps.append(i)
 	print("epoch: %d, error: %f, %g%% complete" % (i, brain.mse(info, goal), percent(i, epochs-1)))
 	
+print("")
+
+plt.plot(errors)
 
 # guessing loop
-for i in range(4):
+for i in range(datasize):
 	data = dataset[i]
 	info = [data[0], data[1]]
 	goal = [data[2]]
-	print(goal, brain.process(info), brain.mse(info, goal))
+	print("answer: %d, guess: %f, error: %s" % (goal[0], brain.process(info)[0], brain.mse(info, goal)))
+
+plt.show()
