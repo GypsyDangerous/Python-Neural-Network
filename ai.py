21ebaba6f910e74dcccecbdf0776249a550712f0
David Snyder <45735850+GypsyDangerous@users.noreply.github.com>
2019-03-16 23:10:46 -0400

    Update ai.py

--- ai.py
+++ ai.py
@@ -1,34 +1,27 @@
 import numpy as np
 from perceptron import *
-import matplotlib.pyplot as plt
-
 
 # simple XOR dataset
-dataset = [[[1,0], [1]],
-			[[0,1], [1]],
-			[[1,1], [0]],
-			[[0,0], [0]]]  
-datasize = len(dataset)
+data = [[1, 0], [0, 1], [1, 1], [0, 0]]
+labels = [[1], [1], [0], [0]]
 
+datasize = len(data)
 # initialize the Neural Network
 brain = perceptron(2, 1, 4, 1, 20000)
-
+epochs = brain.getEpochs()
 brain.setLearningRate(.1)
 
-
 # training loop
-brain.fit(dataset)
+brain.fit(data, labels)
 		
 print("")
 
 # guessing loop
 for i in range(datasize):
-	data = dataset[i]
-	data_len = len(data)-1
-	info = data[0]
-	goal = data[1]
+	info = data[i]
+	goal = labels[i]
 	guess = brain.process(info)
 	error = brain.mse(info, goal)
 	print("answer: %d, guess: %f, error: %s" % (goal[0], 
-						    guess, 
-						    error))
+						guess, 
+						error))
