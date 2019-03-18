c83d942a7ae899153bae09440f56f43eba9eac0e
David Snyder <45735850+GypsyDangerous@users.noreply.github.com>
2019-03-18 07:35:05 -0400

    Update ai.py

--- ai.py
+++ ai.py
@@ -7,7 +7,7 @@ labels = [[1], [1], [0], [0]]
 
 datasize = len(data)
 # initialize the Neural Network
-brain = perceptron(2, 1, 4, 1, 20000)
+brain = perceptron(2, 1, 10, 1, 200000)
 epochs = brain.getEpochs()
 brain.setLearningRate(.1)
 
@@ -23,5 +23,5 @@ for i in range(datasize):
 	guess = brain.process(info)
 	error = brain.mse(info, goal)
 	print("answer: %d, guess: %f, error: %s" % (goal[0], 
-						guess, 
-						error))
+						    guess, 
+						    error))
