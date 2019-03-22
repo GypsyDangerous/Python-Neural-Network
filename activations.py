abbfe030d37b8f30556d3eb2477ec2297b206158
David Snyder <45735850+GypsyDangerous@users.noreply.github.com>
2019-03-21 21:39:17 -0400

    Add files via upload

--- activations.py
+++ activations.py
@@ -1,8 +1,15 @@
 import numpy as np
+
 def sigmoid(x):
 	return 1/(1+np.exp(-x))
 
 def sigmoid_p(x):
+	return x * (1-x)
+
+def softmax(x):
+	return np.exp(x) / np.sum(np.exp(x))
+
+def softmax_p(x):
 	return x * (1-x)
 
 def tanh(x):
@@ -12,7 +19,8 @@ def tanh_p(x):
 	return 1 - x * x
 
 def relu(x):
-	return np.maximum(0, x)
+	return np.maximum(x, 0)
 
 def relu_p(x):
 	return float(x > 0)
+
